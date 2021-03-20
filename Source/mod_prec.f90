e30b359c649e615f65a4b697ffe8370ddb438fae
Justin <lloydripley@gmail.com>
2021-03-20 21:36:38 +0000

    retrying with spherical harmonic library

--- Source/mod_prec.f90
+++
@@ -1,9 +0,0 @@
-!
-! define precision for int, real (and complex) numbers
-!
-module mod_prec
-!   if using fftw use these:
-   use, intrinsic :: iso_c_binding, ip=>c_int, rp=>c_double
-   
-!   use, intrinsic :: iso_fortran_env, only: ip=>int64, rp=>real64
-end module mod_prec
